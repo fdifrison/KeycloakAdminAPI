ccceb04b5fa51f6074559b49936a2539a1ceaa58
gfrison <giovanni.frison@alten.it>
2024-04-18 15:39:33 +0200

    init client project

---
+++ user-roles-client/src/main/java/com/simpl/userroleclient/UserRoleClientApplication.java
@@ -0,0 +1,13 @@
+package com.simpl.userroleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRoleClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRoleClientApplication.class, args);
+	}
+
+}
